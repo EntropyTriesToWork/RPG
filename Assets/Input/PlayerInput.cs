f1314a4af2ee75ce9c737bd0238c193dc717b57f
EntropicEquanimity <64940379+EntropicEquanimity@users.noreply.github.com>
2022-03-09 16:17:28 +0800

    RefactoredMovement

---
+++ Assets/Input/PlayerInput.cs
@@ -0,0 +1,226 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input/New Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""New Controls"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerMovement"",
+            ""id"": ""3d8d2455-8aab-4d52-b704-433d6db0e2c0"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""50b17a1d-57c2-4025-96b5-0022ce91e672"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""ca51d2be-89c4-4d6b-8cb4-a1bd20803b79"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""b3bd899f-bbad-437e-b9ae-70c1ff6da89d"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""92bd3be6-81ad-4de7-b23c-6741c41c0678"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""db8e855e-54a2-4ad9-8734-9c972213ff4f"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""ebe33ca7-92af-4a9d-9c3d-b4e43cb01b58"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""d552e04a-45a8-4a57-b13f-d6a482df4bcc"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""5e71717d-02ef-4b31-a03c-bda13bb9cdbb"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard"",
+            ""bindingGroup"": ""Keyboard"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // PlayerMovement
+        m_PlayerMovement = asset.FindActionMap("PlayerMovement", throwIfNotFound: true);
+        m_PlayerMovement_Movement = m_PlayerMovement.FindAction("Movement", throwIfNotFound: true);
+        m_PlayerMovement_Jump = m_PlayerMovement.FindAction("Jump", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerMovement
+    private readonly InputActionMap m_PlayerMovement;
+    private IPlayerMovementActions m_PlayerMovementActionsCallbackInterface;
+    private readonly InputAction m_PlayerMovement_Movement;
+    private readonly InputAction m_PlayerMovement_Jump;
+    public struct PlayerMovementActions
+    {
+        private @PlayerInput m_Wrapper;
+        public PlayerMovementActions(@PlayerInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_PlayerMovement_Movement;
+        public InputAction @Jump => m_Wrapper.m_PlayerMovement_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerMovement; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerMovementActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerMovementActions instance)
+        {
+            if (m_Wrapper.m_PlayerMovementActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnMovement;
+                @Jump.started -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_PlayerMovementActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public PlayerMovementActions @PlayerMovement => new PlayerMovementActions(this);
+    private int m_KeyboardSchemeIndex = -1;
+    public InputControlScheme KeyboardScheme
+    {
+        get
+        {
+            if (m_KeyboardSchemeIndex == -1) m_KeyboardSchemeIndex = asset.FindControlSchemeIndex("Keyboard");
+            return asset.controlSchemes[m_KeyboardSchemeIndex];
+        }
+    }
+    public interface IPlayerMovementActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,50 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
-    public LayerMask groundLayer, interactablesLayer;
-    private NavMeshAgent _navMeshAgent;
     private Animator _animator;
-    public GameObject clickEffect;
-    public ParticleSystem walkEffect;
-    void Start()
-    {
-        _navMeshAgent = GetComponent<NavMeshAgent>();
-        _animator = GetComponent<Animator>();
-    }
+    private PlayerInput _playerInput;
+    private Camera _camera;
+    private Rigidbody _rb;
 
-    // Update is called once per frame
+    public float MoveSpeed { get => moveSpeed.Value; }
+    public float rotationSpeed;
+
+    public CharacterStat moveSpeed;
+    public LayerMask groundLayer, interactablesLayer;
+
+    public Vector2 movementInput;
+
+    private const string runVar = "Running";
+    private const string msVar = "MoveSpeed";
+
+    private void OnEnable()
+    {
+        _playerInput = new PlayerInput();
+        _playerInput.Enable();
+        _playerInput.PlayerMovement.Movement.performed += playerInput => movementInput = playerInput.ReadValue<Vector2>();
+        _playerInput.PlayerMovement.Movement.canceled += playerInput => movementInput = Vector2.zero;
+        _playerInput.PlayerMovement.Jump.performed += _ => TryToJump();
+
+        _animator = GetComponent<Animator>();
+        _camera = Camera.main;
+        _rb = GetComponent<Rigidbody>();
+    }
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if(movementInput.magnitude <= 0.1f)
         {
-            RaycastHit hitData = RayCastUnderMouse(groundLayer);
-
-            if (hitData.collider != null)
-            {
-                Instantiate(clickEffect, hitData.point, Quaternion.Euler(hitData.normal));
-                Debug.Log(hitData.point);
-                _navMeshAgent.SetDestination(hitData.point);
-            }
+            _animator.SetBool(runVar, false);
         }
-        else if (Input.GetMouseButtonDown(0))
+        else
         {
-            RaycastHit hitData = RayCastUnderMouse(interactablesLayer);
-            if(hitData.collider != null)
-            {
-                hitData.collider.GetComponent<IInteractable>().Interact();
-            }
+            TryToMove();
+            TryToRotate();
+            _animator.SetBool(runVar, true);
         }
-
-        bool isRunning = _navMeshAgent.remainingDistance >= 0.5f;
-        _animator.SetBool("Running", isRunning);
-        if (isRunning && !walkEffect.isPlaying) { walkEffect.Play(); }
-        else if(!isRunning) { walkEffect.Stop(); }
     }
+    #region Movement 
+    public void TryToMove()
+    {
+        _animator.SetFloat(msVar, MoveSpeed);
+        Vector3 moveVector = _camera.transform.forward * movementInput.y;
+        moveVector += _camera.transform.right * movementInput.x;
+        moveVector.Normalize();
 
+        moveVector *= MoveSpeed * 4;
+        moveVector = Vector3.ProjectOnPlane(moveVector, Vector3.up);
+        _rb.velocity = moveVector;
+    }
+    public void TryToRotate()
+    {
+        Vector3 dirVector = _camera.transform.forward * movementInput.y;
+        dirVector += _camera.transform.right * movementInput.x;
+        dirVector.Normalize();
+        dirVector.y = 0;
+
+        Quaternion targetRot = Quaternion.LookRotation(dirVector);
+        Quaternion rot = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * rotationSpeed);
+        transform.rotation = rot;
+    }
+    public void TryToJump()
+    {
+        moveSpeed.AddModifier(new StatModifier(1, StatModType.Flat));
+    }
+    #endregion
+
+    #region Logic
     private RaycastHit RayCastUnderMouse(LayerMask layer)
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -57,4 +87,5 @@ public class PlayerController : MonoBehaviour
     {
 
     }
+    #endregion
 }
